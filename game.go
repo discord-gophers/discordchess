6833a26afbb14fedd81866de7e9279db73c972c0
luisf <stdiopt@gmail.com>
2021-07-09 20:40:24 -0300

    Improvements
    - Add board command
    - Better valid moves info
    - resign actually calls game resign and send outcome
    - remove curID use game state internal oneA
    - Fix reactions
    - simple help command

--- game.go
+++ game.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/notnil/chess"
 )
 
@@ -8,14 +10,31 @@ type game struct {
 	*chess.Game
 
 	whiteID, blackID string
-	curID            string
+	// TODO: {lpf} this can be used later to bust the game if stuck
+	createdAt  time.Time
+	lastMoveAt time.Time
 }
 
 func newGame(whiteID, blackID string) *game {
 	return &game{
 		whiteID: whiteID,
 		blackID: blackID,
-		curID:   whiteID,
-		Game:    chess.NewGame(),
+
+		createdAt:  time.Now().UTC(),
+		lastMoveAt: time.Now().UTC(),
+
+		Game: chess.NewGame(chess.UseNotation(chess.AlgebraicNotation{})),
 	}
+}
+
+func (g *game) MoveStr(m string) error {
+	g.lastMoveAt = time.Now().UTC()
+	return g.Game.MoveStr(m)
+}
+
+func (g *game) turn() string {
+	if g.Position().Turn() == chess.White {
+		return g.whiteID
+	}
+	return g.blackID
 }
